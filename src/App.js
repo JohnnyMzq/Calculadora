c8dcb864ca67578c928a8e74bd22d760de216ddc
JohnnyMzq <89431959+JohnnyMzq@users.noreply.github.com>
2022-02-26 12:40:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+
+
+import './App.css';
+
+function App() {
+
+  //const [number1, setNumber1] = useState("");
+  //const [number2, setNumber2] = useState("");
+  //const [currentOperation, setCurrentOperation] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  }
+
+  const clear = () =>{
+    setResult("")
+  }
+
+  const backspace = () =>{
+    setResult(result.slice(0, result.length -1)) // or -1
+  }
+  
+  const calculate =() =>{
+    setResult(eval(result).toString())
+  }
+  return (
+    <>
+      <h1 className='title'>Calculadora</h1>
+      <div className="container">
+            <form>
+              <input  type="text" value={result} />
+            </form>
+
+          <div className='keypad'>
+          <button className='orange' onClick={clear} id='clear'>AC</button>
+          <button  className='highligth' onClick={backspace} id='backspace'>DEL</button>
+          <button className='highligth' name='/' onClick={handleClick}>&divide;</button>
+          <button name='7' onClick={handleClick}>7</button>
+          <button name='8' onClick={handleClick}>8</button>
+          <button name='9' onClick={handleClick}>9</button>
+          <button className='highligth' name='*' onClick={handleClick}>*</button>
+          <button  name='4' onClick={handleClick}>4</button>
+          <button name='5' onClick={handleClick}>5</button>
+          <button name='6' onClick={handleClick}>6</button>
+          <button className='highligth' name='-' onClick={handleClick}>-</button>
+          <button name='1' onClick={handleClick}>1</button>
+          <button name='2' onClick={handleClick}>2</button>
+          <button name='3' onClick={handleClick}>3</button>
+          <button className='highligth' name='+' onClick={handleClick}>+</button>
+          <button name='0' onClick={handleClick}>0</button>    
+          <button  name='.' onClick={handleClick}>.</button>
+          <button className='highligth' onClick={calculate} id='result'>=</button>
+          </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
